25bc551437437194243d7bc5ac9fd4bcb0b66e66
Nick Ang <nicholas_ang@outlook.com>
2021-09-04 07:11:49 +1000

    update

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :project
   enum status: %i[not_started in_progress completed deleted]
-  enum priority: %i[critical urgent healthy]
+  enum priority: %i[high medium low]
   has_many :task_assigns, dependent: :destroy
   has_many :users, through: :task_assigns
 end
