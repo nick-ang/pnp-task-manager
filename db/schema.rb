f609258d14e468593325205277158c32e80a7964
Nick Ang <nicholas_ang@outlook.com>
2021-09-04 06:56:50 +1000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_03_164832) do
+ActiveRecord::Schema.define(version: 2021_09_03_195506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_09_03_164832) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "created_by"
+    t.integer "priority"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
