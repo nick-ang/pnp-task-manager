b5d178ea55996a0fc05de8cbabbef420a1cc04b0
Fisher Lim <fisher.lim@outlook.com>
2021-09-15 00:55:14 +1000

    Updated Homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
