a38010a71aed5fa48231dbe9e77104bb1a69d985
Nick Ang <nicholas_ang@outlook.com>
2021-09-20 01:50:20 +1000

    Add wikis, style app, add sticky notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_132257) do
+ActiveRecord::Schema.define(version: 2021_09_19_134540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.text "description"
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.string "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "project_assigns", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -83,9 +93,20 @@ ActiveRecord::Schema.define(version: 2021_09_19_132257) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wikis", force: :cascade do |t|
+    t.text "description"
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_wikis_on_user_id"
+  end
+
+  add_foreign_key "notes", "users"
   add_foreign_key "project_assigns", "projects"
   add_foreign_key "project_assigns", "users"
   add_foreign_key "task_assigns", "tasks"
   add_foreign_key "task_assigns", "users"
   add_foreign_key "tasks", "projects"
+  add_foreign_key "wikis", "users"
 end
---
+++ test/controllers/wikis_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WikisControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
