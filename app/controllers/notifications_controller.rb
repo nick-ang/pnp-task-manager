d7c27f7b8c8468bee46bf0683b073a43a59054cd
Nick Ang <nicholas_ang@outlook.com>
2021-09-23 18:45:24 +1000

    up to heroku

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,6 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notification = Notification.new
     @notifications = current_user.notifications
   end
 end
