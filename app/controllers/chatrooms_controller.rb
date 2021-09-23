bc04202af2fce5011cdff93991594c4088cab8dc
Nick Ang <nicholas_ang@outlook.com>
2021-09-23 19:24:05 +1000

    enable chatroom delete

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -22,6 +22,18 @@ class ChatroomsController < ApplicationController
     @message = Message.new
   end
 
+  def destroy
+    @chatroom = Chatroom.find(params[:id])
+    @notifications = Notification.all
+    @notifications.each do |notification|
+      if Notification.first.params[:message].chatroom_id == @chatroom.id
+        notification.destroy
+      end
+    end
+    @chatroom.destroy
+    redirect_to chatrooms_path
+  end
+
   def chatroom_params
     params.require(:chatroom).permit(:name, user_ids: [])
   end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,6 @@
 class Chatroom < ApplicationRecord
   has_many :chatroom_assigns, dependent: :destroy
   has_many :users, through: :chatroom_assigns
-  has_many :messages
+  has_many :messages, dependent: :destroy
   # has_and_belongs_to_many :users
 end
