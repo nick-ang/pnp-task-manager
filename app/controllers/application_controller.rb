fe23ad047a75ab64b2e1279b9b4474c992b2df98
Nick Ang <nicholas_ang@outlook.com>
2021-09-30 12:27:29 +1000

    add default user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  before_action :demo_user
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -11,5 +12,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  def demo_user
+    if User.all.length > 4
+      @demo = User.find(4)
+    else
+      @demo = User.first
+    end
   end
 end
