da893cfa96398a11d18484090f196c10197c1123
Nick Ang <nicholas_ang@outlook.com>
2021-10-01 09:38:24 +1000

    fix notification error

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -23,6 +23,7 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @project_new = Project.new
   end
 
   def destroy
