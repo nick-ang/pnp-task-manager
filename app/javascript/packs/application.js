9e5c61b699a5fc1f450a3a839a5dde855396b540
Fisher Lim <fisher.lim@outlook.com>
2021-10-23 23:19:08 +1100

    Updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -53,3 +53,13 @@ $('form').on('focus', 'input[type=number]', function (e) {
 $('form').on('blur', 'input[type=number]', function (e) {
   $(this).off('wheel.disableScroll')
 })
+
+$('#editProject').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('projectid') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.test').text('Project Id' + recipient)
+  //modal.find('.modal-body input').val(recipient)
+})
